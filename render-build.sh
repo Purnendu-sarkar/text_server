5041a0bae5756b52ba52f0a3dee0878f3f294dea
Purnendu Sarkar <dsr102.purnendu@gmail.com>
2025-12-12 00:39:02 +0600

    Final

--- render-build.sh
+++ render-build.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 set -o errexit
 
-npm install
-npm run build      # <-- dist/ generate
+# Install dependencies
+npm install --legacy-peer-deps
+
+# Compile TypeScript
+npm run build
+
+# Prisma generate & push
 npm run db:generate
 npm run db:push
